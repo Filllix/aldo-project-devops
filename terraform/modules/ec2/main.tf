9b4a6f9d2eed0611836c1628daa162a0d5a1e6d0
aldocloud <aldo059805@gmail.com>
2026-04-19 03:39:21 +0700

    initial clean project

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "web_sg" {
+name = "devops-web-sg"
+vpc_id = var.vpc_id
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+resource "aws_instance" "web" {
+ami = var.ami
+instance_type = var.instance_type
+key_name = var.key_name
+
+subnet_id = var.subnet_id
+vpc_security_group_ids = [aws_security_group.web_sg.id]
+associate_public_ip_address = true
+
+user_data = <<-EOF
+#!/bin/bash
+apt update -y
+apt install docker.io -y
+systemctl start docker
+systemctl enable docker
+EOF
+
+tags = {
+Name = "devops-ec2"
+}
+}
