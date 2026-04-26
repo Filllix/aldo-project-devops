a39b543d80de0c4819b03034da2d93b60a18d701
aldocloud <aldo059805@gmail.com>
2026-04-26 21:49:39 +0700

    update security group add prometheus 9090

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -10,8 +10,29 @@ cidr_blocks = ["0.0.0.0/0"]
 }
 
 ingress {
+from_port = 8080
+to_port = 8080
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 8081
+to_port = 8081
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
 from_port = 80
 to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 9090
+to_port = 9090
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
